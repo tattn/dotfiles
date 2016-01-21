44c901bae877cbbe0b3d0d7f08de0afd2ccd7f37
Tatsuya Tanaka (tattn) <tatsuyars@yahoo.co.jp>
2016-01-21 19:30:18 +0900

    Update a trend script

--- lib/trend.rb
+++ lib/trend.rb
@@ -8,11 +8,37 @@ rescue LoadError
   exit 1
 end
 
+class String
+  def full_length() count("^ -~｡-ﾟ") end
+  def length_ja() length + full_length end
+  def ljust_ja(width, padstr=' ') align_ja(:ljust, width, padstr) end
+  def rjust_ja(width, padstr=' ') align_ja(:rjust, width, padstr) end
+  def center_ja(width, padstr=' ') align_ja(:center, width, padstr) end
+
+  def align_ja(method, width, padstr, dummy='A'*length_ja)
+    if full_length == 0
+      send(method, width, padstr)
+    else
+      dummy.succ!.empty? && break while padstr.include?(dummy)
+      dummy.send(method, width, padstr).sub(dummy, self)
+    end
+  end
+end
+
 agent = Mechanize.new
 agent.user_agent_alias = "Linux Firefox"
 page = agent.get('http://searchranking.yahoo.co.jp/rt_burst_ranking/')
 list = page / '//*[@id="main"]/div[1]/ul'
-words = list.search('a').map {|word| word.text}
-words.each_with_index do |word, i|
-  puts "#{i+1}. #{word}"
+burst_words = list.search('a').map {|word| word.text}
+
+page = agent.get('http://news.yahoo.co.jp/list')
+list = page / '//*[@id="main"]/div[1]/div[1]/div[4]/ul'
+news = list.search('a span[class=ttl]').map {|title| title.text}
+
+width = 25
+puts '-' * (width*2 + 5)
+puts "#{'急上昇ワード'.center_ja(width)} | #{'ニュース'.center_ja(width)}"
+puts '-' * (width*2 + 5)
+burst_words.zip(news).each do |burst_word, news_title|
+  puts "#{burst_word.center_ja width} | #{news_title.center_ja width}"
 end
